837771da26be293dd5fc132b9318a9e7c51c92b6
Mpho Maleka <mpho.maleka3@gmail.com>
2023-07-18 14:28:03 +0200

    import cv2

--- analytics/api/views.py
+++ analytics/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, mixins
 from rest_framework.views import APIView
 import datetime
+import cv2
 
 import requests
-
 
 
 from analytics.models import pingservor
@@ -19,16 +19,14 @@ class pingservorPost(generics.CreateAPIView):
     serializer_class = pingservorSerializers
 
 
-
 # class ScannerDataSerilisersAPIView(
-#     mixins.CreateModelMixin, 
+#     mixins.CreateModelMixin,
 #     generics.ListAPIView,
 #     ):
 
 
 #     lookup_field = 'id'
 #     serializer_class =  ScannerDataDataSerialisersv1
-
 
 
 #     def get_queryset(self):
@@ -40,9 +38,9 @@ class pingservorPost(generics.CreateAPIView):
 #         serializer = ScannerDataDataSerialisersv1(data=request.data)
 #         if serializer.is_valid():
 #             self.post_server(
-#                 serializer.data['shell_no'], 
-#                 serializer.data['cast_code'], 
-#                 serializer.data['heat_code'], 
-#                 serializer.data['shell_serial_no'], 
+#                 serializer.data['shell_no'],
+#                 serializer.data['cast_code'],
+#                 serializer.data['heat_code'],
+#                 serializer.data['shell_serial_no'],
 #                 serializer.data['location'])
 #         return self.create(request, *args, **kwargs)
